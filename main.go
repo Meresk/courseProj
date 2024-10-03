f3134823370cbfc0208a600a2883de0cc698a8e8
Яков Хватов <YakovX95@mail.ru>
2024-10-04 00:43:53 +0300

    Немного комментов

--- main.go
+++ main.go
@@ -18,6 +18,61 @@ var register = make(chan *websocket.Conn)
 var broadcast = make(chan string)
 var unregister = make(chan *websocket.Conn)
 
+func main() {
+	app := fiber.New()
+
+	// Страница на / будет home.html
+	app.Static("/", "./home.html")
+
+	/* Мидлваер с помощью use в контором определена функция с параметром c у которой тип - ссылка на объект fiber.Ctx
+	ctx - context - Это структура, предоставляемая библиотекой Fiber, которая содержит всю информацию о текущем HTTP-запросе
+	и предоставляет методы для работы с ним.
+	*/
+	app.Use(func(c *fiber.Ctx) error {
+
+		// функция считывает заголовки http для определенния содержит ли он Upgrade и Connection, которые являются обязательными для веб-сокетов.
+		if websocket.IsWebSocketUpgrade(c) {
+			//log.Println(c.Request())
+			return c.Next()
+		}
+		return c.SendStatus(fiber.StatusUpgradeRequired)
+	})
+
+	// запуск горутины (ОДНОЙ)
+	go runHub()
+
+	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
+		defer func() {
+			unregister <- c
+			c.Close()
+		}()
+
+		register <- c
+
+		for {
+			messageType, message, err := c.ReadMessage()
+			if err != nil {
+				if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+					log.Println("read error:", err)
+				}
+
+				return
+			}
+
+			if messageType == websocket.TextMessage {
+				broadcast <- string(message)
+
+			} else {
+				log.Println("webcosket message received of type", messageType)
+			}
+		}
+	}))
+
+	addr := flag.String("addr", ":8080", "http service address")
+	flag.Parse()
+	log.Fatal(app.Listen(*addr))
+}
+
 func runHub() {
 	for {
 		select {
@@ -25,6 +80,7 @@ func runHub() {
 			clients[connection] = &client{}
 			log.Println("connection registered")
 
+		// Когда в broadcast что-то появлиось
 		case message := <-broadcast:
 			log.Println("message received", message)
 			// Отправка сообщения всем клиентам
@@ -53,49 +109,4 @@ func runHub() {
 			log.Println("connection unregistered")
 		}
 	}
-}
-
-func main() {
-	app := fiber.New()
-
-	app.Static("/", "./home.html")
-
-	app.Use(func(c *fiber.Ctx) error {
-		if websocket.IsWebSocketUpgrade(c) {
-			return c.Next()
-		}
-		return c.SendStatus(fiber.StatusUpgradeRequired)
-	})
-
-	go runHub()
-
-	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
-		defer func() {
-			unregister <- c
-			c.Close()
-		}()
-
-		register <- c
-
-		for {
-			messageType, message, err := c.ReadMessage()
-			if err != nil {
-				if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-					log.Println("read error:", err)
-				}
-
-				return
-			}
-
-			if messageType == websocket.TextMessage {
-				broadcast <- string(message)
-			} else {
-				log.Println("webcosket message received of type", messageType)
-			}
-		}
-	}))
-
-	addr := flag.String("addr", ":8080", "http service address")
-	flag.Parse()
-	log.Fatal(app.Listen(*addr))
 }
